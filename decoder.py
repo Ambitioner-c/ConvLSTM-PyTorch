65d353b4e2f3f0d7979a93a18a419a134109926f
Cui <656359504@qq.com>
2023-02-13 14:46:44 +0800

    理解decoder

--- decoder.py
+++ decoder.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File    :   decoder.py
 @Time    :   2020/03/09
 @Author  :   jhhuang96
 @Mail    :   hjh096@126.com
 @Version :   1.0
 @Description:   decoder
-'''
+"""
 
 from torch import nn
 from utils import make_layers
 import torch
+
+from net_params import convlstm_encoder_params, convlstm_decoder_params
+from data.mm import MovingMNIST
+from encoder import Encoder
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 class Decoder(nn.Module):
@@ -26,7 +33,8 @@ class Decoder(nn.Module):
             setattr(self, 'stage' + str(self.blocks - index),
                     make_layers(params))
 
-    def forward_by_stage(self, inputs, state, subnet, rnn):
+    @staticmethod
+    def forward_by_stage(inputs, state, subnet, rnn):
         inputs, state_stage = rnn(inputs, state, seq_len=10)
         seq_number, batch_size, input_channel, height, width = inputs.size()
         inputs = torch.reshape(inputs, (-1, input_channel, height, width))
@@ -49,35 +57,33 @@ class Decoder(nn.Module):
         return inputs
 
 
-if __name__ == "__main__":
-    from net_params import convlstm_encoder_params, convlstm_forecaster_params
-    from data.mm import MovingMNIST
-    from encoder import Encoder
-    import os
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
+def main():
     encoder = Encoder(convlstm_encoder_params[0],
                       convlstm_encoder_params[1]).cuda()
-    decoder = Decoder(convlstm_forecaster_params[0],
-                      convlstm_forecaster_params[1]).cuda()
+    decoder = Decoder(convlstm_decoder_params[0],
+                      convlstm_decoder_params[1]).cuda()
     if torch.cuda.device_count() > 1:
         encoder = nn.DataParallel(encoder)
         decoder = nn.DataParallel(decoder)
 
-    trainFolder = MovingMNIST(is_train=True,
-                              root='data/',
-                              n_frames_input=10,
-                              n_frames_output=10,
-                              num_objects=[3])
-    trainLoader = torch.utils.data.DataLoader(
-        trainFolder,
+    train_folder = MovingMNIST(is_train=True,
+                               root='data/',
+                               n_frames_input=10,
+                               n_frames_output=10,
+                               num_objects=[3])
+    train_loader = torch.utils.data.DataLoader(
+        train_folder,
         batch_size=8,
         shuffle=False,
     )
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    for i, (idx, targetVar, inputVar, _, _) in enumerate(trainLoader):
+    for i, (idx, targetVar, inputVar, _, _) in enumerate(train_loader):
         inputs = inputVar.to(device)  # B,S,1,64,64
         state = encoder(inputs)
         break
     output = decoder(state)
     print(output.shape)  # S,B,1,64,64
+
+
+if __name__ == "__main__":
+    main()
